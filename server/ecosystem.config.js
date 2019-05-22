dead5e58ccf2151771b8ed1e6238fccc3c9e27d6
danztran <ntctheboy97@gmail.com>
2019-05-22 17:31:07 +0700

    add: README.md guide

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -11,7 +11,7 @@ const envConfig = {
 			'storage',
 			'.git',
 			'logs',
-			process.env.VUE_DIST
+			process.env.VUE_DIST || 'vue-dist'
 		],
 		log_date_format: 'YYYY-MM-DD HH:mm Z',
 		out_file: './logs/out.log',
@@ -21,9 +21,9 @@ const envConfig = {
 
 module.exports = {
 	apps: [{
-		name: process.env.SUB_NAME,
-		script: process.env.MAIN_JS,
-		...envConfig[process.env.NODE_ENV],
+		name: process.env.SUB_NAME || 'dev',
+		script: process.env.MAIN_JS || './bin/www',
+		...envConfig[process.env.NODE_ENV || 'development'],
 		instances: process.env.PM2_INSTANCES || 1,
 		exec_mode: 'cluster',
 		autorestart: true,
